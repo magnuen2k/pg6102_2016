0bf121399a5a5512852a6b74b2fe4b9e92497d2e
magnuen2k <maen030@student.kristiania.no>
2021-12-14 12:48:42 +0100

    setup gateway

---
+++ api-gateway/src/main/kotlin/no/kristiania/apigateway/Application.kt
@@ -0,0 +1,15 @@
+package no.kristiania.apigateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class Application {
+}
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
